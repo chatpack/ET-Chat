687393ba19682b49f9e0f6f33e42d4398e2bb854
chatpack <chatpack@users.noreply.github.com>
2018-10-02 01:59:05 +0200

    Generate index.html
    Let webpack create index.html from template.

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require( "html-webpack-plugin" );
+
 module.exports = {
 
   // Chosen mode tells webpack to use its built-in optimizations accordingly.
-  mode: "production", // "production" | "development" | "none"
-                      // "production": enable many optimizations for production builds
-                      // "development": enabled useful tools for development
-                      // "none": no defaults
+  // ... inactive. mode is passed in to webpack by option. See package.json scripts.
+  // mode: "production", // "production" | "development" | "none"
 
   // The base directory, an absolute path, for resolving entry points and loaders from configuration.
   // By default the current directory is used, but it's recommended to pass a value in your configuration. 
@@ -21,20 +21,8 @@ module.exports = {
   //   a: "./app/entry-a",
   //   b: ["./app/entry-b1", "./app/entry-b2"]
   // },
-  entry: { 
-    index:  "./src/login",
-    chat:   [ "./src/chat",
-              "./src/window"         // chatwindows
-            ],
-    vendor: [ "./src/prototype",     // required by scriptaculous
-              "./src/scriptaculous", // the highness itself
-              "./src/builder",       // required by scriptaculous
-              "./src/effects",       // required by scriptaculous
-              "./src/dragdrop",      // required by scriptaculous
-              "./src/controls",      // required by scriptaculous
-              "./src/slider",        // required by scriptaculous
-              "./src/sound"          // required by scriptaculous
-           ]
+  entry: {
+    polyfill:   path.resolve( __dirname, "..", "..", "src", "polyfill.js" )
   },
   // options related to how webpack emits results
   output: {
@@ -59,17 +47,17 @@ module.exports = {
     // library: "packed", // string,
 
     // the type of the exported library
-    // libraryTarget: "umd", // universal module definition
-    // libraryTarget: "umd2", // universal module definition
+    // libraryTarget: "umd",       // universal module definition
+    // libraryTarget: "umd2",      // universal module definition
     // libraryTarget: "commonjs2", // exported with module.exports
-    // libraryTarget: "commonjs", // exported as properties to exports
-    // libraryTarget: "amd", // defined with AMD defined method
-    // libraryTarget: "this", // property set on this
-    // libraryTarget: "var", // variable defined in root scope
-    // libraryTarget: "assign", // blind assignment
-    // libraryTarget: "window", // property set to window object
-    // libraryTarget: "global", // property set to global object
-    libraryTarget: "jsonp", // jsonp wrapper
+    // libraryTarget: "commonjs",  // exported as properties to exports
+    // libraryTarget: "amd",       // defined with AMD defined method
+    // libraryTarget: "this",      // property set on this
+    // libraryTarget: "var",       // variable defined in root scope
+    // libraryTarget: "assign",    // blind assignment
+    // libraryTarget: "window",    // property set to window object
+    // libraryTarget: "global",    // property set to global object
+    // libraryTarget: "jsonp",     // jsonp wrapper
 
     // include useful path info about modules, exports, requests, etc. into the generated cod
     pathinfo: true, // boolean
@@ -114,24 +102,28 @@ module.exports = {
     // prefix module sources in bundle for better readablitity
     // sourcePrefix: "\t", // string
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template:   path.resolve( __dirname, "..", "..", "src", "index.ejs" ),
+      title:      "Change Title",
+      inject:     false,
+      minify:     false,
+      app:        {
+                    version:  "4.0.0"
+                  }
+    })
+  ],
   module: {
     rules: [
       {
-        test: /\.js?$/,
-        enforce: 'pre',
-        exclude: [ /node_modules/, /deploy/ ],
-        use: [
-          {
-            loader:  'prettier-loader',
-            options: {
-              parser: "babylon" , // The default prettier parser (we might want 'flow' in future)
-              printWidth: 80,     // Specify the length of line that the printer will wrap on.
-              tabWidth: 2,        // Specify the number of spaces per indentation-level.
-              useTabs: false,     // Indent lines with tabs instead of spaces.
-              semi: true          // Print semicolons at the ends of statements.
-            }
-          }
-        ]
+          test: /\.html$/,
+          exclude: [ /node_modules/, /deploy/ ],
+          use:     [{
+                   loader:  "html-loader",
+                   options: {
+                              minimize: false
+                            }
+                 }]
       }
     ]
   },
