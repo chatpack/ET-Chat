8170fbed149e61dcce36236d4536ac363c1e7380
chatpack <43591370+chatpack@users.noreply.github.com>
2018-10-08 10:42:51 +0200

    update
    tried some codepages to get rid of crap characters in webpack output.
    Did not work. Fails documented.

--- env.bat
+++ env.bat
@@ -1,5 +1,12 @@
 @ECHO off
 
+REM Get rid of bullshit chars in webpack output. 
+REM Appearently the following codepages do not work
+REM
+REM CHCP 65001
+REM CHCP 473
+REM CHCP 1252
+
 SET Path=%CD%\node_v8.12.0_win-x64
 
 ECHO Environment:
